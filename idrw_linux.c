abb729ec03a8423ad14927ce7774b73bfa989dec
Adam Laurie <adam@algroup.co.uk>
2016-04-25 20:29:04 +0200

    idrw_linux: probe for usb descriptor
    This is needed to setup the reader in working mode. Before you
    had to run "sudo lsusb -v" to get the reader working.

--- idrw_linux.c
+++ idrw_linux.c
@@ -103,6 +103,7 @@ https://github.com/Simpleyyt/libfunction_so_usb
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <getopt.h>
+#include <unistd.h>
 #include <libusb-1.0/libusb.h> 
 
 #define VENDOR_ID 0xffff
@@ -566,6 +567,21 @@ int main(int argc,char** argv)
         fprintf(stderr, "libusb_claim_interface error %d\n", r); 
         goto out; 
     }
+
+    // addy experimental - wakeup device as per lsusb -v
+    libusb_device *dev;
+    dev=  libusb_get_device(devh);
+    struct libusb_config_descriptor *config;
+    r = libusb_get_config_descriptor(dev, 0, &config);
+    if (r < 0) {
+        fprintf(stderr, "libusb_get_config_descriptor error %d\n", r);
+        goto out;
+    }
+    char tmp[1];
+    r = libusb_control_transfer(devh,LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_STANDARD | LIBUSB_RECIPIENT_INTERFACE,LIBUSB_REQUEST_GET_DESCRIPTOR,(LIBUSB_DT_REPORT << 8),0,tmp,1,TIMEOUT);
+    libusb_reset_device(devh);
+    sleep(2);
+
     /* USB init done */
 
 
